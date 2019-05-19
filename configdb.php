0bef498bf117ff1fcb67b4f6fe3379af67e6ff54
irvanardiansyah12 <50799710+irvanardiansyah12@users.noreply.github.com>
2019-05-19 21:10:02 +0700

    Add files via upload

---
+++ configdb.php
@@ -0,0 +1,11 @@
+<?php
+	@session_start();
+	$_SESSION['judul'] = 'SPK WP GADGET';
+	$_SESSION['welcome'] = 'SISTEM PENGAMBIL KEPUTUSAN BERBASIS WEB DENGAN METODE WEIGHT PRODUCT';
+	$_SESSION['by'] = 'Irvan Ardiansyah';
+	$mysqli = new mysqli('localhost','root','','spk-wp');
+	if($mysqli->connect_errno){
+		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
+		exit();
+	}
+?>
